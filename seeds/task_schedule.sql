daf773f7d026376b35b17184300138ac7b5629e7
farisdewantoro <farisd117@gmail.com>
2025-07-07 19:13:09 +0700

    feat(seeds): jobs schedule seed

---
+++ seeds/task_schedule.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.task_schedules
+(id, job_id, cron_expression, next_execution, last_execution, is_active, created_at, updated_at)
+VALUES(7, 4, '*/3 9-16 * * 1-5', '2025-07-07 09:00:00.000', '2025-07-04 16:57:00.695', true, '2025-06-17 20:44:35.328', '2025-07-04 16:57:00.701');
+INSERT INTO public.task_schedules
+(id, job_id, cron_expression, next_execution, last_execution, is_active, created_at, updated_at)
+VALUES(1, 1, '0 8,11,15 * * 1-5', '2025-07-07 08:00:00.000', '2025-07-04 15:00:00.711', true, '2025-06-17 06:51:27.898', '2025-07-04 15:00:00.715');
+INSERT INTO public.task_schedules
+(id, job_id, cron_expression, next_execution, last_execution, is_active, created_at, updated_at)
+VALUES(8, 5, '10 8,12,15 * * 1-5', '2025-07-07 08:10:00.000', '2025-07-04 15:10:00.692', true, '2025-06-23 16:57:18.362', '2025-07-04 15:10:00.696');
+INSERT INTO public.task_schedules
+(id, job_id, cron_expression, next_execution, last_execution, is_active, created_at, updated_at)
+VALUES(3, 3, '30 8,11,15 * * 1-5', '2025-07-07 08:30:00.000', '2025-07-04 15:30:00.694', true, '2025-06-17 07:36:03.830', '2025-07-04 15:30:00.699');
+INSERT INTO public.task_schedules
+(id, job_id, cron_expression, next_execution, last_execution, is_active, created_at, updated_at)
+VALUES(10, 7, '0 8-16 * * 1-5', '2025-07-07 08:00:00.000', '2025-07-04 16:00:00.693', true, '2025-06-24 13:16:48.060', '2025-07-04 16:00:00.699');
